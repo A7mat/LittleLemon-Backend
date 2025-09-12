e19b68225ddce636e246773d8ea3bddcbf614ec7
a7mat <ahmadasadi37@gmail.com>
2025-09-12 21:34:51 +0200

    set up MySQL connection

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'LittleLemon',  
+        'USER': 'root',  
+        'PASSWORD': 'a7961396',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+
     }
 }
 
