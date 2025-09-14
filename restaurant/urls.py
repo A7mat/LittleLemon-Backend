70e3de56a9b9decc1ae414e86921eddd58cd5d87
a7mat <ahmadasadi37@gmail.com>
2025-09-14 13:50:05 +0200

    add registration page + secure booking api (djoser)

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import MenuItemView, SingleMenuItemView #, BookingView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # path('', views.index, name='index'),
     path('menu/', MenuItemView.as_view(), name='menu'),
     path('menu/<int:pk>', SingleMenuItemView.as_view(), name='menu'),
     # path('booking/', BookingView.as_view(), name='booking'),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-
+# from django.shortcuts import render
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
+from django.contrib.auth.models import User
+
 from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
@@ -13,19 +16,23 @@ from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 # Create your views here.
 # def index(request):
 #     return render(request, 'index.html', {})
+
 class UserViewSet(ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
    permission_classes = [permissions.IsAuthenticated]
 
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-        
+
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
