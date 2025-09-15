c55787aa12e31bb3e4097f6763167a702d98e57f
a7mat <ahmadasadi37@gmail.com>
2025-09-15 20:01:59 +0200

    adjust permissions for API calls
    only allow authenticated users to add, adjust or delete menu items

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,12 +3,11 @@
 # from rest_framework.response import Response
 from django.contrib.auth.models import User
 
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
@@ -20,17 +19,20 @@ from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 class UserViewSet(ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated]
+   permission_classes = [IsAuthenticated]
 
 class MenuItemView(ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookingViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
